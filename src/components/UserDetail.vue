beac0edf2e211bd0360ed903440b0fffdcce4571
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-12 09:18:20 +0700

    update UserDetail

---
+++ src/components/UserDetail.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="component">
+        <h3>Information User Details</h3>
+        <p>List Details</p>
+        <p>My name: {{customName}}</p>
+        <p>My age: {{userAge}}</p>
+        <button @click="resetName">Reset Name</button>
+        <button @click="resetNameFn()">Reset case: Call Back</button>
+    </div>
+</template>
+
+<script>
+    //props dùng để truyền dữ liệu từ component cha sang các component con,
+    //và các con truyền thông báo đến component cha thông qua các sự kiện
+    export default {
+        props: {
+            customName: {
+                type: String,
+                default: 'sao lai lo roi'
+            },
+            resetNameFn: Function,
+            userAge: Number
+        },
+        methods: {
+            resetName() {
+                this.customName = 'Khuynh Thanh';
+                this.$emit('nameWasReset', this.customName);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
