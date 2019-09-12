dd84647dc2840a303e64d2600da848d8663046e8
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-12 10:25:59 +0700

    create eventBus

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import User from "@/components/User";
 import App from './App.vue'
 Vue.component('appUser', User);
-
+export const eventBus = new Vue();
 new Vue({
   render: h => h(App),
 }).$mount('#app');
