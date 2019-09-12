b63b876d939958f95c28dc3b6775545568e874c2
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-12 10:37:46 +0700

    update and refactor eventBus

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import User from "@/components/User";
 import App from './App.vue'
 Vue.component('appUser', User);
-export const eventBus = new Vue();
+export const eventBus = new Vue({
+    data: {
+
+    },
+    methods: {
+        changeAge(age) {
+            this.$emit('ageWasEdit', age);
+        }
+    }
+});
 new Vue({
   render: h => h(App),
 }).$mount('#app');
